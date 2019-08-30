f396f1b2a47836ee9bea4c291e83be8e741d78d1
Antonia Atanasova <antoniyaata@gmail.com>
2019-08-31 01:08:39 +0300

    Add navigation styles

--- soft-wiki/src/Components/Common/Navigation/Navigation.js
+++ soft-wiki/src/Components/Common/Navigation/Navigation.js
@@ -1,30 +1,46 @@
-import React from 'react';
-import './Navigation.scss';
+import React from "react";
+import "./Navigation.scss";
 
-import { Switch, Route, NavLink } from 'react-router-dom';
+import { Switch, Route, NavLink } from "react-router-dom";
 
-import Python from '../../Technologies/Python/Python.js';
-import Java from '../../Technologies/Java/Java.js';
-import CSharp from '../../Technologies/CSharp/CSharp.js';
-import JavaScript from '../../Technologies/JavaScript/JavaScript.js';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+import Python from "../../Technologies/Python/Python.js";
+import Java from "../../Technologies/Java/Java.js";
+import CSharp from "../../Technologies/CSharp/CSharp.js";
+import JavaScript from "../../Technologies/JavaScript/JavaScript.js";
 
 function Navigation() {
-    return (
-        <div className="navigation">
-            <div className='menu-container'>
-                <NavLink to="/python">Python</NavLink>
-                <NavLink to="/java">Java</NavLink>
-                <NavLink to="/javascript">JavaScript</NavLink>
-                <NavLink to="/csharp">C#</NavLink>
-                <Switch>
-                    <Route path="/python" component={Python}></Route>
-                    <Route path="/csharp" component={CSharp}></Route>
-                    <Route path="/java" component={Java}></Route>
-                    <Route path="/javascript" component={JavaScript}></Route>
-                </Switch>
-            </div>
+  const faHomeIcon = <FontAwesomeIcon icon={faHome} />;
+  const faSearchIcon = <FontAwesomeIcon icon={faSearch} />;
+
+  return (
+    <div className="Navigation">
+      <div className="menu-container">
+        <div className="menu-items">
+          <NavLink to="/python">
+            <span>{faHomeIcon}</span>
+          </NavLink>
+          <NavLink to="/python">Python</NavLink>
+          <NavLink to="/java">Java</NavLink>
+          <NavLink to="/javascript">JavaScript</NavLink>
+          <NavLink to="/csharp">C#</NavLink>
+          <Switch>
+            <Route path="/python" component={Python}></Route>
+            <Route path="/csharp" component={CSharp}></Route>
+            <Route path="/java" component={Java}></Route>
+            <Route path="/javascript" component={JavaScript}></Route>
+          </Switch>
         </div>
-    )
+
+        <div className="search-bar">
+          <span>{faSearchIcon}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navigation;
