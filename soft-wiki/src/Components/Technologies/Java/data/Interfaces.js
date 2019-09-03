0fdf434ed14c414b3ab9197eab7cf6ea331f74fb
DimitarTanasi <mitko_107@abv.bg>
2019-09-03 15:37:34 +0300

    create Encapsulation.js file, add some classes to Technology.scss

--- soft-wiki/src/Components/Technologies/Java/data/Interfaces.js
+++ soft-wiki/src/Components/Technologies/Java/data/Interfaces.js
@@ -5,6 +5,7 @@ function Interfaces() {
     return (
         <div>
             <article>
+                <h1>Interfaces</h1>
                 <p>An interface is a reference type in Java. It is similar to class. It is a collection of abstract methods. A class implements an interface, thereby inheriting the abstract methods of the interface.</p>
                 <p>Along with abstract methods, an interface may also contain constants, default methods, static methods, and nested types. Method bodies exist only for default methods and static methods.</p>
                 <p>Writing an interface is similar to writing a class. But a class describes the attributes and behaviors of an object. And an interface contains behaviors that a class implements.</p>
