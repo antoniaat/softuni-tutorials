9f8e4466264324c32d191e0e572ee7d865d1d07d
tany1610 <t.staneva97@abv.bg>
2019-09-03 16:45:15 +0300

    add numeric types in python

--- soft-wiki/src/Components/Technologies/Python/data/ForStatement.js
+++ soft-wiki/src/Components/Technologies/Python/data/ForStatement.js
@@ -1,11 +1,63 @@
 import React from "react";
+import { exportNamespaceSpecifier } from "@babel/types";
 // import "../Technology.scss";
 
 function ForStatement() {
     return (
         <div>
             <h1>For Statement</h1>
-            
+            <p>A <b>for</b> loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).</p>
+            <p>This is less like the <b>for</b> keyword in other programming languages, and works more like an iterator method as found in other object-orientated programming languages.</p>
+            <p>With the <b>for</b> loop we can execute a set of statements, once for each item in a list, tuple, set etc.</p>
+            <div className='code-block'>
+                <code>
+                    <p>fruits = ["apple", "banana", "cherry"]</p>
+                    <p>for x in fruits:</p>
+                    <p>&emsp;print(x)</p>
+                </code>
+            </div>
+            <p>The <b>for</b> loop does not require an indexing variable to set beforehand.</p>
+            <h2><u>Looping through a string</u></h2>
+            <p>Even strings are <i>iterable</i> objects, they contain a sequence of characters:</p>
+            <div className='code-block'>
+                <code>
+                    <p>for x in "banana":</p>
+                    <p>&esmp;print(x)</p>
+                </code>
+            </div>
+            <h2><u>The break statement</u></h2>
+            <p>With the <b>break</b> statement we can stop the loop before it has looped through all the items:</p>
+            <div className='code-block'>
+                <code>
+                    <p>fruits = ["apple", "banana", "cherry"]</p>
+                    <p>for x in fruits:</p>
+                    <p>&emsp;print(x)</p>
+                    <p>&emsp;if x == "banana":</p>
+                    <p>&emsp;&emsp;break</p>
+                </code>
+            </div>
+            <br></br>
+            <div className='code-block'>
+                <code>
+                    <p>fruits = ["apple", "banana", "cherry"]</p>
+                    <p>for x in fruits:</p>
+                    <p>&emsp;print(x)</p>
+                    <p>&emsp;if x == "banana":</p>
+                    <p>&emsp;&emsp;break</p>
+                    <p>&emsp;print(x)</p>
+                </code>
+            </div>
+            <h2><u>The continue statement</u></h2>
+            <p>With the <b>continue</b> statement we can stop the current iteration of the loop, and continue with the next:</p>
+            <div className='code-block'>
+                <code>
+                    <p>fruits = ["apple", "banana", "cherry"]</p>
+                    <p>for x in fruits:</p>
+                    <p>&emsp;if x == "banana"</p>
+                    <p>&emsp;&emsp;continue</p>
+                    <p>&emsp;print(x)</p>
+                </code>
+            </div>
         </div>
     )
 }
--- soft-wiki/src/Components/Technologies/Python/data/PythonNumber.js
+++ soft-wiki/src/Components/Technologies/Python/data/PythonNumber.js
@@ -4,27 +4,12 @@ import React from "react";
 function PythonNumber() {
     return (
         <div>
-            <h1>Assignment Statements</h1>
-            <p>Assignment statements are used to (re)bind names to values and to modify attributes or items of mutable objects:</p>
-            <div className='code-block'>
-                <code>
-                <p><strong>assignment_stmt</strong> ::=  (target_list "=")+ (starred_expression | yield_expression) </p>
-                <p><strong>target_list</strong>     ::=  target ("," target)* [","] </p>
-                <p><strong>target</strong>          ::=  identifier</p>
-                <p>                    | "(" [target_list] ")"</p>
-                <p>                    | "[" [target_list] "]"</p>
-                <p>                    | attributeref</p>
-                <p>                    | subscription</p>
-                <p>                    | slicing</p>
-                <p>                    | "*" target</p>
-                </code>
-            </div>
-            <p>(See section Primaries for the syntax definitions for <i>attributeref, subscription, and slicing</i>.)</p>
-            <p>An assignment statement evaluates the expression list (remember that this can be a single expression or a comma-separated list, the latter yielding a tuple) and assigns the single resulting object to each of the target lists, from left to right.</p>
-            <p>Assignment is defined recursively depending on the form of the target (list). When a target is part of a mutable object (an attribute reference, subscription or slicing), the mutable object must ultimately perform the assignment and decide about its validity, and may raise an exception if the assignment is unacceptable. The rules observed by various types and the exceptions raised are given with the definition of the object types (see section The standard type hierarchy).</p>
-            <h4>Assignment of an object to a target list, optionally enclosed in parentheses or square brackets, is recursively defined as follows.</h4>
-            <li>If the target list is a single target with no trailing comma, optionally in parentheses, the object is assigned to that target.</li>
-            <li>Else: The object must be an iterable with the same number of items as there are targets in the target list, and the items are assigned, from left to right, to the corresponding targets.</li>
+            <h1>Numeric Types in Python</h1>
+            <p>In any OOP language, there are many different data types. In Python, number data types are used to store numeric values. There are four different numerical types in Python:</p>
+            <li><b>int</b> (plain integers): this one is pretty standard -- plain integers are just positive or negative whole numbers.</li><br></br>
+            <li><b>long</b> (long integers): long integers are integers of infinite size. They look just like plain integers except they're followed by the letter "L" (ex: 150L).</li><br></br>
+            <li><b>float</b> (floating point real values): floats represent real numbers, but are written with decimal points (or scientific notaion) to divide the whole number into fractional parts.</li><br></br>
+            <li><b>complex</b> (complex numbers): represented by the formula a + bJ, where a and b are floats, and J is the square root of -1 (the result of which is an imaginary number). Complex numbers are used sparingly in Python.</li><br></br>
         </div>
     )
 }
