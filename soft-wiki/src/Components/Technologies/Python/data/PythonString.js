31f632d3192c46daa7b48fb18ae8d64118fdec46
tany1610 <t.staneva97@abv.bg>
2019-09-04 17:13:33 +0300

    add repl

--- soft-wiki/src/Components/Technologies/Python/data/PythonString.js
+++ soft-wiki/src/Components/Technologies/Python/data/PythonString.js
@@ -10,20 +10,10 @@ function PythonString() {
             <p><b><i>'hello'</i></b> is the same as <b><i>"hello"</i></b>.</p>
 
             <p>You can display a string literal with the <b><i>print()</i></b> function:</p>
-            <div className="code-block">
-                <code>
-                    <p>print("Hello")</p>
-                    <p>print('Hello')</p>
-                </code>
-            </div>
+            <iframe height="400px" width="100%" src="https://repl.it/repls/ZanyWorriedFtpclient?lite=true" scrolling="no" frameborder="no" allowtransparency="true" allowfullscreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"></iframe>
             <h2><u>Assign string to a variable</u></h2>
             <p>Assigning a string to a variable is done with the variable name followed by an equal sign and the string:</p>
-            <div className="code-block">
-                <code>
-                    <p>a = "Hello"</p>
-                    <p>print(a)</p>
-                </code>
-            </div>
+            <iframe height="400px" width="100%" src="https://repl.it/repls/WonderfulEuphoricWatchdog?lite=true" scrolling="no" frameborder="no" allowtransparency="true" allowfullscreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"></iframe>
             <h2><u>Multiline strings</u></h2>
             <p>You can assign a multiline string to a variable by using three quotes:</p>
             <div className="code-block">
